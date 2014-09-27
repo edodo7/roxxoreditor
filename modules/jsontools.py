e03bceb53cff35f71a6dd82b0b5fa36b2ee2ef7e
Alexandre Devaux <ADevaux42@gmail.com>
2014-09-27 19:57:56 +0200

    Add 'write' function into 'jsontools'.

--- modules/jsontools.py
+++ modules/jsontools.py
@@ -3,9 +3,6 @@
 
 import json
 
-# Global variables
-global content
-
 def read(filename):
 	""" Reads the file specified by the filename and returns the content
 	as a python dict.
@@ -13,11 +10,17 @@ def read(filename):
 	Keyword arguments:
 	filename -- The name of the file with the contents to process.
 	"""
-	content = json.loads(filename)
+	return json.loads(filename)
 
-	return content
 
-def selectItem():
+def write(filename, content):
+	""" Writes the given content into the specified filename.
+
+	Keyword arguments:
+	filename -- The name of the file in which the contents will be write.
+	content  -- The content to write into the file.
 	"""
-	"""
-	# ToDo
+	str_dump = json.dumps(content, indent=4, separators=(',', ': '))
+
+	with open(filename, 'w') as foutput:
+		foutput.writelines(str_dump)
