4250df5f1c320d63b36a2e348b0fa27d41d841ca
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2014-10-12 19:55:55 +0200

    Add cleanDataStructure function.

--- modules/json/tools.py
+++ modules/json/tools.py
@@ -12,3 +12,44 @@ def extractDataStructure(dataStruct, path: list):
     for i in range(len(path)):
         dataStruct = dataStruct[path[i]]
     return dataStruct
+
+def cleanDataStructure(dataStruct):
+    """ Walk around the entire data structure dataStruct and set all the data
+        to a default value.
+        Default values:
+        int   -> 0
+        float -> 0.0
+        None  -> None
+        str   -> ""
+
+    Keyword arguments:
+        dataStruct -- The data structure to clean.
+    """
+    def cleanData(data):
+        """ Return a default value according to the type of data.
+
+        Keyword arguments:
+        data -- The data to get the default value.
+        """
+        if data == None:
+            return None
+        elif type(data) == int:
+            return 0
+        elif type(data) == float:
+            return 0.0
+        elif type(data) == str:
+            return ""
+
+    if type(dataStruct) == list:
+        for i in range(len(dataStruct)):
+            if type(dataStruct[i]) == list or type(dataStruct[i]) == dict:
+                cleanDataStructure(dataStruct[i])
+            else:
+                dataStruct[i] = cleanData(dataStruct[i])
+    else:
+        keyList = dataStruct.keys()
+        for key in keyList:
+            if type(dataStruct[key]) == list or type(dataStruct[key]) == dict:
+                cleanDataStructure(dataStruct[key])
+            else:
+                dataStruct[key] = cleanData(dataStruct[key])
